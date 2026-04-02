008893645b509b1f539930fec6a25ec9f7e660d4
Pratik Manure <162863265+Pratikmanure@users.noreply.github.com>
2026-04-02 23:25:27 +0530

    Update and rename JOINS to Joins/students_and_marks.sql

---
+++ Joins/students_and_marks.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Students (
+    id INT,
+    name VARCHAR(50)
+);
+
+INSERT INTO Students VALUES
+(1, 'Amit'),
+(2, 'Neha'),
+(3, 'Ravi');
+
+CREATE TABLE Marks (
+    studentId INT,
+    marks INT
+);
+
+INSERT INTO Marks VALUES
+(1, 85),
+(2, 90);
+
