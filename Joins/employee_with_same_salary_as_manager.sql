033639bbb7542b3a8ead0d5fe40f34b099e48eb0
Pratik Manure <162863265+Pratikmanure@users.noreply.github.com>
2026-04-02 23:32:13 +0530

    Update employee_with_same_salary_as_manager.sql

--- Joins/employee_with_same_salary_as_manager.sql
+++ Joins/employee_with_same_salary_as_manager.sql
@@ -9,3 +9,12 @@ INSERT INTO EmployeeSalary VALUES
 (1, 'Amit', 50000, 2),
 (2, 'Rohit', 50000, NULL),
 (3, 'Neha', 60000, 2);
+
+--answer 
+
+SELECT 
+    e.name AS employee_name,
+    m.name AS manager_name
+FROM Employee e
+LEFT JOIN Employee m
+ON e.managerId = m.id;
