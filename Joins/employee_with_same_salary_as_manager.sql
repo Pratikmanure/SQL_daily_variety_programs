2304e4f7ba949bf0aad751afed209bec5293c0a1
Pratik Manure <162863265+Pratikmanure@users.noreply.github.com>
2026-04-02 23:28:47 +0530

    Create employee_with_same_salary_as_manager.sql

---
+++ Joins/employee_with_same_salary_as_manager.sql
@@ -0,0 +1,11 @@
+CREATE TABLE EmployeeSalary (
+    id INT,
+    name VARCHAR(50),
+    salary INT,
+    managerId INT
+);
+
+INSERT INTO EmployeeSalary VALUES
+(1, 'Amit', 50000, 2),
+(2, 'Rohit', 50000, NULL),
+(3, 'Neha', 60000, 2);
